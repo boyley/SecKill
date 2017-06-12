141980678f74358d6b784459320041b618f4173c
lenovo <136397156@qq.com>
2017-06-12 22:27:09 +0800

    商品缓存

--- modules/seckill-service/src/main/java/com/danlu/seckill/service/impl/SeckillServiceImpl.java
+++ modules/seckill-service/src/main/java/com/danlu/seckill/service/impl/SeckillServiceImpl.java
@@ -27,9 +27,6 @@ public class SeckillServiceImpl implements SeckillService {
     private SeckillRepository seckillRepository;
 
     @Autowired
-    private ApplicationContext applicationContext;
-
-    @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
     @Override
