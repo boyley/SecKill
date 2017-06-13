8527636e44732060d654665a0654777be0551324
lenovo <136397156@qq.com>
2017-06-13 21:51:10 +0800

    常用地址

--- modules/seckill-service/src/main/resources/data.sql
+++ modules/seckill-service/src/main/resources/data.sql
@@ -6,3 +6,6 @@ INSERT INTO `goods` VALUES ('4', '商品标题4', '商品名称4', '商品摘要
 INSERT INTO `seckill` VALUES ('1', '1', '2017-06-11 14:34:28', '2017-06-12 14:34:31', '11');
 INSERT INTO `seckill` VALUES ('2', '2', '2017-06-11 14:34:28', '2017-06-16 14:34:31', '11');
 INSERT INTO `seckill` VALUES ('3', '3', '2017-06-11 14:34:28', '2017-06-12 14:34:31', '11');
+
+INSERT INTO `receiver_address` VALUES ('1', '1', '四川省 成都市 高新区 环球中心', '\0', '2017-06-13 21:47:33');
+INSERT INTO `receiver_address` VALUES ('2', '1', '四川省 成都市 高新区 双流县', '', '2017-06-13 21:47:41');
--- modules/seckill-service/src/main/resources/schema.sql
+++ modules/seckill-service/src/main/resources/schema.sql
@@ -3,14 +3,16 @@ CREATE DATABASE seckill DEFAULT CHARACTER SET UTF8;
 
 USE seckill;
 
-drop table if exists goods;
+DROP TABLE IF EXISTS `goods`;
 
-drop table if exists seckill;
+DROP TABLE IF EXISTS `seckill`;
+
+DROP TABLE IF EXISTS `receiver_address`;
 
 /*==============================================================*/
 /* Table: goods                                                 */
 /*==============================================================*/
-DROP TABLE IF EXISTS `goods`;
+
 CREATE TABLE `goods` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `title` varchar(20) DEFAULT NULL COMMENT '商品标题',
@@ -24,14 +26,24 @@ CREATE TABLE `goods` (
 /*==============================================================*/
 /* Table: seckill                                               */
 /*==============================================================*/
-create table seckill
-(
-   id                   int not null auto_increment comment '主键',
-   goods_id             int comment '商品id',
-   start_time           datetime comment '秒杀开始时间',
-   end_time             datetime comment '秒杀结束时间',
-   stock                int comment '库存',
-   primary key (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE TABLE `seckill` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `goods_id` int(11) DEFAULT NULL COMMENT '商品id',
+  `start_time` datetime DEFAULT NULL COMMENT '秒杀开始时间',
+  `end_time` datetime DEFAULT NULL COMMENT '秒杀结束时间',
+  `stock` int(11) DEFAULT NULL COMMENT '库存',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='秒杀活动';
 
-alter table seckill comment '秒杀活动';
+
+-- ----------------------------
+-- Table structure for seckill
+-- ----------------------------
+CREATE TABLE `receiver_address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户ID',
+  `address` varchar(50) DEFAULT NULL COMMENT '地址',
+  `use` bit(1) DEFAULT b'0' COMMENT '是否是常用地址',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='收货地址';
